53e70872070f27e45face66f0858a4c67ae5442f
Caio José <caiojsdl16@gmail.com>
2025-10-17 17:50:30 -0300

    Corrigido a questão do slider

--- src/app/slider/slider.ts
+++ src/app/slider/slider.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './slider.html',
   styleUrls: ['./slider.css']
 })
 export class Slider {
-
+  images = [944, 1011, 984].map((n) => `images/slide1.png`);
 }
