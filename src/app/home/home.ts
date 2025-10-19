0393efe81ef06af26c137e00605330ea945672d2
Caio José <caiojsdl16@gmail.com>
2025-10-19 10:53:03 -0300

    feito o componente de produtos

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MenuSuperior } from '../menu-superior/menu-superior';
 import { Slider } from '../slider/slider';
 import { InfoOfertas } from '../info-ofertas/info-ofertas';
+import { Produtos } from '../produtos/produtos';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuSuperior, Slider, InfoOfertas],
+  imports: [MenuSuperior, Slider, InfoOfertas, Produtos],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
