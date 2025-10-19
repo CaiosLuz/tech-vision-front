9f04fde1c66a26da1f66f75d0842b3be45213d02
Caio José <caiojsdl16@gmail.com>
2025-10-19 11:02:11 -0300

    feito o footer

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,11 +3,12 @@ import { MenuSuperior } from '../menu-superior/menu-superior';
 import { Slider } from '../slider/slider';
 import { InfoOfertas } from '../info-ofertas/info-ofertas';
 import { Produtos } from '../produtos/produtos';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuSuperior, Slider, InfoOfertas, Produtos],
+  imports: [MenuSuperior, Slider, InfoOfertas, Produtos, Footer],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
